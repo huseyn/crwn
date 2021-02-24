ef96cf459a215687aa7d4bc65ced5c45ff2d1251
Huseyn <huseyn.mikayil95@gmail.com>
2021-02-24 13:36:34 +0400

    Create shoppage and collection-preview component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import HomePage from "./pages/homepage/homepage";
+import HomePage from "./pages/home/homepage";
+import ShopPage from "./pages/shop/shoppage";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
