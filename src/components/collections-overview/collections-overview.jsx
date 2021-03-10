f3537032c549e697285c6ef1ab3a027739ec6c52
Huseyn Mikayil <huseyn.mikayil95@gmail.com>
2021-03-10 12:02:39 +0400

    Normalize shop collection data by converting shop data array to object

--- src/components/collections-overview/collections-overview.jsx
+++ src/components/collections-overview/collections-overview.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import { selectShopCollectionsForPreview } from "../../redux/shop/shop.selectors";
 
 import CollectionPreview from "../collection-preview/collection-preview";
 
 import "./collections-overview.scss";
 
 const CollectionsOverview = ({ collections }) => {
+  console.log("collections: ", collections);
   return (
-    <div className='collections-overview'>
+    <div className="collections-overview">
       {collections.map(({ id, ...rest }) => (
         <CollectionPreview key={id} {...rest} />
       ))}
@@ -19,7 +20,7 @@ const CollectionsOverview = ({ collections }) => {
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
+  collections: selectShopCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item";
 
 import "./collection.scss";
 
-const Collection = ({ match }) => {
-  return <div>Collection Page: {match.params.collectionId}</div>;
+import { selectShopCollection } from "../../redux/shop/shop.selectors";
+
+const Collection = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Collection;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectShopCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(Collection);
